aacae83ac1cc6a6ef0017f3d91be55b26327a375
David Sevcik <david.sevcik@gmail.com>
2013-07-15 22:45:17 +0200

    make image precompilation working on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ Todohq::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.initialize_on_precompile = false
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
   # Generate digests for assets URLs.
   config.assets.digest = true
